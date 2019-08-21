0803ce472ceb97db5972adeda91c762803ecdf85
germanio7 <german.e.escobar@gmail.com>
2019-08-21 10:59:50 -0300

    show group

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,9 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('chats', 'API\ChatsController', ['only' => ['index', 'store']]);
     Route::get('getMessage', 'API\ChatsController@fetchMessages');
     Route::post('send', 'API\ChatsController@sendMessages');
+
+    /*Grupos*/
+    Route::apiResource('groups', 'API\GruposController', ['only' => ['show']]);
 });
 
 
